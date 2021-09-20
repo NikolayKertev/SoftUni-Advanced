8c8e6330c0137afba9e11ef2b6dd2f8c05e6c704
NikolayKertev <nikolaikertev@abv.bg>
2021-09-20 19:46:28 +0300

    Homework

---
+++ Stacks and Queues/09. Simple Text Editor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            Stack<string> textStack = new Stack<string>();
+            textStack.Push(sb.ToString());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                if (input[0] == "1")
+                {
+                    sb.Append(input[1]);
+                    string text = sb.ToString();
+                    textStack.Push(text);
+                }
+                else if (input[0] == "2")
+                {
+                    string text = sb.ToString();
+                    text = text.Substring(0, text.Length - int.Parse(input[1]));
+                    sb.Clear();
+                    sb.Append(text);
+                    text = sb.ToString();
+                    textStack.Push(text);
+                }
+                else if (input[0] == "3")
+                {
+                    int index = int.Parse(input[1]);
+                    Console.WriteLine(sb[index - 1]);
+                }
+                else if (input[0] == "4")
+                {
+                    textStack.Pop();
+                    sb.Clear();
+                    string text = textStack.Peek();
+                    sb.Append(text);
+                }
+            }
+        }
+    }
+}
