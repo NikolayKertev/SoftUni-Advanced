71b6367859c8027b17554981b6032de6d7727b59
NikolayKertev <nikolaikertev@abv.bg>
2021-09-28 12:49:13 +0300

    Homework

---
+++ Sets and Dictionaries Advanced/05. Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var characters = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!characters.ContainsKey(text[i]))
+                {
+                    characters.Add(text[i], 0);
+                }
+
+                characters[text[i]]++;
+            }
+
+            foreach (var ch in characters)
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}
