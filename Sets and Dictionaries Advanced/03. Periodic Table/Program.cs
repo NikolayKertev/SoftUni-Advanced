6b743dd7463b0433bf9ac425861994295fc5b41d
NikolayKertev <nikolaikertev@abv.bg>
2021-09-28 12:48:53 +0300

    Homework

---
+++ Sets and Dictionaries Advanced/03. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            var elements = new SortedSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    elements.Add(input[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
