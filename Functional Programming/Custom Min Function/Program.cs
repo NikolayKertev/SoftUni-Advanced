81c98ee086090d1730e32741bca0e5c02eb0df0b
NikolayKertev <nikolaikertev@abv.bg>
2022-10-12 15:57:14 +0300

    Homework

---
+++ Functional Programming/Custom Min Function/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<HashSet<int>, int> min = (numbers) =>
+            {
+                int min = int.MaxValue;
+
+                foreach (var number in numbers)
+                {
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                }
+
+                return min;
+            };
+
+            HashSet<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+
+            Console.WriteLine(min(numbers));
+        }
+    }
+}
